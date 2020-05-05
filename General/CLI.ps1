b0a7618a58fb054a948a3ed9547c30a8f45aad15
Mihai Sarbulescu <mihai.sarbulescu@gmail.com>
2020-05-05 20:20:26 +0300

    fds

--- General/CLI.ps1
+++
@@ -1,9 +0,0 @@
-foreach($product in Get-ChildItem "$scriptPath\Products")
-{
-    if($productList.Contains($product)) {
-        try {
-            . "$scriptPath\Products\$($product.Name)\CLI.ps1"
-        }
-        catch { }
-    }
-}
